485522087917f48112dd1e9bcd9744a000fa4a6b
Briana Phillips <briana.eden.phillips@gmail.com>
2023-08-10 09:52:06 -0500

    PostHome created

---
+++ src/components/Posts/PostHome.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { getAllPosts } from "../../managers/PostManager"
+
+export const PostList = () => {
+    const [posts, setPosts] = useState([])
+    //const navigate = useNavigate()
+
+
+    const localRareUser = localStorage.getItem("rare_user")
+    const rareUserObject = JSON.parse(localRareUser)
+
+    useEffect(() => {
+        getAllPosts().then(data => setPosts(data))
+    }, [])
+
+    //useEffect hook to filter by specific state- in this case, emergency status
+
+    return <>
+
+        <h2>List of Posts</h2>
+
+        <article className="posts">
+            {
+                posts.map(
+                    (posts) => {
+                        return <section className="posts">
+                            <header>{posts.title}</header>
+                            <body>{posts.author}</body>
+                            <footer>{posts.category}</footer>
+                        </section>
+                    }
+                )
+            }
+        </article>
+    </>
+}
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
+import { PostList } from "../components/Posts/PostHome"
 import { Authorized } from "./Authorized"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
     <Routes>
-      <Route path="/login" element={<Login setToken={setToken} />}  />
-      <Route path="/register" element={<Register setToken={setToken} />}  />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="/register" element={<Register setToken={setToken} />} />
+      <Route path="/posts" element={<PostList />} />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
-        
+
       </Route>
     </Routes>
   </>
